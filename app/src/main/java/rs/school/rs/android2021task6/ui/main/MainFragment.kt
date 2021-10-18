a39a7cc8142bf7a30432a3b4063c0e8ebd943a4e
avm82 <1982alexvm@gmail.com>
2021-10-18 17:57:33 +0300

    Initial commit

---
+++ app/src/main/java/rs/school/rs/android2021task6/ui/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package rs.school.rs.android2021task6.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import rs.school.rs.android2021task6.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/rs/school/rs/android2021task6/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package rs.school.rs.android2021task6.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
