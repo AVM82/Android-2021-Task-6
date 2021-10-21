09a99fe5abbe148a0c5147b27a2b3388a1f5d057
avm82 <1982alexvm@gmail.com>
2021-10-21 21:57:32 +0300

    add parseJSON

--- core/src/main/java/rs/school/rs/core/utils/ParseJSON.kt
+++ core/src/main/java/rs/school/rs/core/utils/ParseJSON.kt
@@ -1,28 +1,26 @@
 package rs.school.rs.core.utils
 
-import android.util.Log
 import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import rs.school.rs.core.model.Song
-
+import java.util.Collections
 
 class ParseJSON {
-
+    @Throws(JSONException::class)
     fun toSongList(json: String): List<Song> {
-
-        var list = emptyList<Song>()
+        val mutableSongList: MutableList<Song> = arrayListOf()
         val jsonArray = JSONArray(json)
         for (i in 0 until jsonArray.length()) {
-            val song = jsonArray.getJSONObject(i)
-            Log.e("json", "$i=$song")
-            val s = Song(
-                title = song.getString("title"),
-                artist = song.getString("artist"),
-                bitmapUri = song.getString("bitmapUri"),
-                trackUri = song.getString("trackUri"))
-            Log.e("song", "$s")
-            list = list + s
+            mutableSongList.add(toSong(jsonArray.getJSONObject(i)))
         }
-        return list
+        return Collections.unmodifiableList(mutableSongList)
     }
 
+    private fun toSong(jsonObj: JSONObject) = Song(
+        title = jsonObj.getString("title"),
+        artist = jsonObj.getString("artist"),
+        bitmapUri = jsonObj.getString("bitmapUri"),
+        trackUri = jsonObj.getString("trackUri")
+    )
 }
